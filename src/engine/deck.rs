672960e4b2ab1e0f237397ac0bdb40327adefa2f
michael <mfdeakin@cs.unc.edu>
2021-07-10 18:21:46 -0400

    Add objectives and the cow deck

--- src/engine/deck.rs
+++ src/engine/deck.rs
@@ -1,7 +1,8 @@
-
 use serde::{Serialize, Deserialize};
-use crate::actions::ActionTag;
+use crate::actions::{ActionTag, ActionValues};
 use crate::player::Player;
+use crate::logical::And;
+use crate::buildings::Tepee::Blue;
 
 #[derive(Copy, Clone, Eq, PartialEq, Serialize, Deserialize, Debug)]
 pub enum Card {
@@ -30,7 +31,21 @@ pub struct Cow {
 
 impl Cow {
     pub fn new(color: CowColor, points: u32) -> Cow {
-        Cow {color, points}
+        Cow { color, points }
+    }
+
+    pub fn defaultCowDeck() -> Vec<Cow> {
+        vec![
+            vec![Cow::new(CowColor::Holstein, 1); 7],
+            vec![Cow::new(CowColor::Swiss, 2); 7],
+            vec![Cow::new(CowColor::Ayrshire, 3); 7],
+            vec![Cow::new(CowColor::Highland, 3); 3],
+            vec![Cow::new(CowColor::Highland, 4); 3],
+            vec![Cow::new(CowColor::Highland, 5); 3],
+            vec![Cow::new(CowColor::Longhorn, 5); 2],
+            vec![Cow::new(CowColor::Longhorn, 6); 2],
+            vec![Cow::new(CowColor::Longhorn, 7); 2],
+        ].into_iter().flatten().collect::<Vec<Cow>>()
     }
 
     pub fn value(&self) -> u32 {
@@ -45,6 +60,15 @@ impl Cow {
             CowColor::Highland => 4,
             CowColor::Longhorn => 5,
         }
+    }
+
+    pub fn playerStartingDeck() -> Vec<Cow> {
+        let mut deck = Vec::from([Cow::new(CowColor::Jersey, 0); 5]);
+        deck.append(&mut Vec::from([Cow::new(CowColor::Dutch, 0); 3]));
+        deck.append(&mut Vec::from([Cow::new(CowColor::Guernsey, 0); 3]));
+        deck.append(&mut Vec::from([Cow::new(CowColor::Angus, 0); 3]));
+
+        return deck;
     }
 }
 
@@ -73,20 +97,82 @@ pub enum ObjectiveRequirements {
     SanFran,
     GreenTepee,
     BlueTepee,
-    PlacedDisc,
-    Cow(u32),
+    StationDisc,
+    Cow(u32), // The cows value
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Serialize, Deserialize, Debug)]
 pub struct Objective {
-    immediate: ActionTag,
+    immediate: Option<ActionTag>,
     success_pts: u32,
     fail_pts: u32,
-    requirements: [Option<ObjectiveRequirements>; 4],
+    requirements: And<ObjectiveRequirements, 4>,
 }
 
 impl Objective {
-    fn meets_requirements(&self, player: Player) -> bool {
+    pub fn new(immediate: Option<ActionTag>, success_pts: u32, fail_pts: u32,
+           requirements: &[ObjectiveRequirements]) -> Objective {
+        Objective { immediate, success_pts, fail_pts, requirements: And::new(requirements) }
+    }
+
+    pub fn meetsRequirements(&self, player: Player) -> bool {
         panic!("Not implemented");
+    }
+
+    pub fn baseObjectives() -> Vec<Objective> {
+        vec![Objective::new(None, 3, 0,
+                            &[ObjectiveRequirements::Cow(3), ObjectiveRequirements::Cow(4), ObjectiveRequirements::Building, ]),
+             Objective::new(None, 3, 0,
+                            &[ObjectiveRequirements::BlueTepee, ObjectiveRequirements::Hazard, ObjectiveRequirements::Hazard, ]),
+             Objective::new(None, 3, 0,
+                            &[ObjectiveRequirements::StationDisc, ObjectiveRequirements::StationDisc, ObjectiveRequirements::GreenTepee, ]),
+             Objective::new(None, 3, 0,
+                            &[ObjectiveRequirements::Building, ObjectiveRequirements::Building, ObjectiveRequirements::Hazard, ]),
+        ]
+    }
+
+    pub fn objectives() -> Vec<Objective> {
+        let teleport = Some(ActionTag::TeleportCattleman(ActionValues::Exact(3)));
+        let move_eng_2 = Some(ActionTag::MoveEngine(ActionValues::Exact(2)));
+        let move_eng_3 = Some(ActionTag::MoveEngine(ActionValues::Exact(3)));
+        let dbl_aux = Some(ActionTag::DoubleAuxiliary);
+        let take_coins = Some(ActionTag::TakeCoins(ActionValues::Exact(2)));
+        let draw_cards = Some(ActionTag::DrawCards(ActionValues::Exact(3)));
+
+        let cow_3 = ObjectiveRequirements::Cow(3);
+        let cow_4 = ObjectiveRequirements::Cow(4);
+        let cow_5 = ObjectiveRequirements::Cow(5);
+        use ObjectiveRequirements::StationDisc;
+        use ObjectiveRequirements::GreenTepee;
+        use ObjectiveRequirements::BlueTepee;
+        use ObjectiveRequirements::Hazard;
+        use ObjectiveRequirements::Building;
+        use ObjectiveRequirements::SanFran;
+        vec![
+            Objective::new(dbl_aux, 5, 3, &[SanFran,]),
+            Objective::new(dbl_aux, 5, 3, &[SanFran,]),
+            Objective::new(dbl_aux, 5, 3, &[SanFran,]),
+            Objective::new(dbl_aux, 5, 3, &[SanFran,]),
+            Objective::new(draw_cards, 4, 2, &[cow_3, cow_3, cow_3, StationDisc,]),
+            Objective::new(take_coins, 4, 2, &[cow_3, cow_3, cow_3, Building,]),
+            Objective::new(move_eng_2, 5, 3, &[cow_3, cow_4, cow_5,]),
+            Objective::new(teleport, 5, 2, &[cow_3, cow_4, cow_5,]),
+            Objective::new(teleport, 5, 2, &[cow_3, cow_4, Hazard, Hazard,]),
+            Objective::new(take_coins, 3, 2, &[cow_4, Hazard, Hazard,]),
+            Objective::new(move_eng_2, 5, 3, &[cow_4, cow_4, StationDisc, GreenTepee,]),
+            Objective::new(draw_cards, 3, 2, &[cow_5, Hazard,]),
+            Objective::new(take_coins, 3, 2, &[StationDisc, StationDisc, Hazard,]),
+            Objective::new(move_eng_3, 5, 3, &[StationDisc, StationDisc, Hazard, Hazard,]),
+            Objective::new(teleport, 5, 2, &[StationDisc, StationDisc, Building, Building,]),
+            Objective::new(teleport, 5, 2, &[StationDisc, StationDisc, BlueTepee, BlueTepee,]),
+            Objective::new(draw_cards, 3, 2, &[StationDisc, GreenTepee, GreenTepee,]),
+            Objective::new(draw_cards, 3, 2, &[StationDisc, GreenTepee, BlueTepee,]),
+            Objective::new(draw_cards, 3, 2, &[Building, Building, Hazard,]),
+            Objective::new(teleport, 5, 2, &[Building, Building, Hazard, Hazard,]),
+            Objective::new(move_eng_2, 5, 3, &[Building, Building, GreenTepee, GreenTepee,]),
+            Objective::new(move_eng_3, 5, 3, &[Building, BlueTepee, Hazard, Hazard,]),
+            Objective::new(take_coins, 3, 2, &[Building, BlueTepee, BlueTepee,]),
+            Objective::new(take_coins, 3, 2, &[Building, GreenTepee, BlueTepee,]),
+        ]
     }
 }
